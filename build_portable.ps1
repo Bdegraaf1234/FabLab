fe5c1a253d2833a03ee4ddbf5b81bab449c95198
Bastiaan de Graaf <bdegraaf1234@gmail.com>
2023-06-08 13:58:54 +0200

    autoversioning

--- build_portable.ps1
+++ build_portable.ps1
@@ -4,10 +4,11 @@
 # first we clear the nuget caches so we don't have old dependencies in there. This might be gratuitous but doesn't hurt
 dotnet nuget locals all --clear
 
-# then we build the projects on local config. This builds all projects. 
+# then we clean the old files, restore the nuget packages and build the projects on local config. This builds all projects. 
 # After FabLab.csproj is built, all non-system dlls in the output folder are copied to .\NugetBuilder\folder for packing
 # when dependencies.csproj is built directly afterwards, these dlls are packed into a nuget (name: dependencies, version taken from dependencies.csproj)
 # this nuget ends up in the .\nuget
+dotnet clean /p:configuration=local
 dotnet restore /p:configuration=local
 dotnet build /p:configuration=local
 
